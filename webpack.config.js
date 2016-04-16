d098880dd6040bf5d1ec25092a3a380584bb6796
park junho <huinalam@gmail.com>
2016-04-16 20:55:38 +0900

    [추가] webpack 적용

---
+++ webpack.config.js
@@ -0,0 +1,35 @@
+﻿var path = require('path');
+var webpack = require('webpack');
+
+
+var config = {
+    entry: [
+        "./entry.js",
+        'webpack/hot/dev-server',
+        'webpack-dev-server/client?http://localhost:8000/',
+    ],
+    output: {
+        path: __dirname,
+        filename: "bundle.js"
+    },
+    //devServer: {
+    //    contentBase: ".",
+    //    host: "localhost",
+    //    port: 9000
+    //},
+    module: {
+        loaders: [
+            {
+                test: /\.css$/,
+                loader: "style!css"
+            }, {
+                test: /\.styl$/,
+                loader: 'style-loader!css-loader!stylus-loader'
+            }
+        ]
+    },
+    plugins: [
+      new webpack.HotModuleReplacementPlugin(),
+    ]
+};
+module.exports = config;
