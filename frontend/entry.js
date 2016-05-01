50de592aa1837d74836c36e99ab94134e0d6d6e7
park junho <huinalam@gmail.com>
2016-05-02 01:36:33 +0900

    [추가] 1200x700 해상도 아래시 sorry 페이지 적용

--- frontend/entry.js
+++ frontend/entry.js
@@ -3,4 +3,8 @@ inner_chart();
 
 require("../js/article_scroll.js");
 require("../css/article.styl");
+
+require("./size_cut_off.js");
+require("../css/size_cut_off.styl");
+
 //require("./frontend/wt_navibar.js");
