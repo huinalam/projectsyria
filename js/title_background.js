e6e3c0937785a5bd77a5cb9f1f2eee51c1b188bd
groovelb <groovelb@gmail.com>
2016-12-14 00:07:35 +0900

    no message

--- js/title_background.js
+++ js/title_background.js
@@ -1,6 +1,6 @@
 var title_width = parseInt(d3.select(".header_background").style('width')) *0.8;
 var title_height = title_width;
-var title_margin = title_width *0.1;
+var title_margin = title_width *0.05;
 var title_canvas_width = title_width - 2*title_margin;
 var title_canvas_height = title_height - 2*title_margin;
 
@@ -22,6 +22,7 @@ var dy = Math.random()*range - range/2;
 //README 원과 라인의 좌표를 맞추기 위한 random move원 좌표 배열 
 var ix = [];
 var iy = [];
+var total_num=0;
 
 //push_circle(circle_num);
 //initial_animation();
@@ -92,8 +93,52 @@ function random_move(time){
 		}
 
 }
+function scroll_move(){
+
+		
+		//시작점, 끝점 인덱스
+		for(i=0; i<total_num; i++){
+			//원 트랜지션
+			var dx = Math.random()*range - range/2;
+		 	var dy = Math.random()*range - range/2;
+
+		 	ix[i] = parseFloat(d3.select(".circle"+i).attr("cx")) +dx;
+		 	iy[i] = parseFloat(d3.select(".circle"+i).attr("cy")) +dx;
+
+		 	d3.select(".circle" +i)
+		 		  .transition()
+		 		  .duration(600)
+		 		  .ease("elastic")
+		 		  .attr("cx",ix[i])
+		 		  .attr("cy",iy[i]);
+		 }
+
+		 d3.selectAll(".m_line")
+		   .transition()
+		   .duration(600)
+		   .ease("elastic")
+		   .attr("x1",function(d){
+		   	 var index = d.s;
+		   	 return ix[index];
+		   })
+		   .attr("y1",function(d){
+		   	 var index = d.s;
+		   	 return iy[index];
+		   })
+		   .attr("x2",function(d){
+		   	 var index = d.e;
+		   	 return ix[index];
+		   })
+		   .attr("y2",function(d){
+		   	 var index = d.e;
+		   	 return iy[index];
+		   });
+
+
+}
 
 function first_drawing(num){
+	total_num+= num;
 
 	for(i=0;i<num;i++){
 		var temp = {};
@@ -103,31 +148,33 @@ function first_drawing(num){
 		circle_coord.push(temp);
 	}
 
-	for(i=0; i<num; i++){
-	 	for(j=i+1; j<num; j++){
+	var index =0;
+	for(i=0; i<total_num; i++){
+	 	for(j=i+1; j<total_num; j++){
 	 		var temp = {};
 	 		temp.s = i;
 	 		temp.e = j;
-	 		line_coord.push(temp);
+	 		line_coord[index]= temp;
+	 		index+=1;
 	 	}
 	 }
 
 
 	title_g.selectAll("circle")
-	   .data(circle_coord)
-	   .enter()
-	   .append("circle")
-	   .attr("class",function(d,i){
-	   	 return "m_circle" + " " + "circle" +i;
-	   })
-	   .attr("cx",function(d){
-	   		return d.x;
-	   })
-	   .attr("cy",function(d){
-	   		return d.y;
-	   })
-	   .attr("r","0.3em")
-	   .style("opacity",0);
+		   .data(circle_coord)
+		   .enter()
+		   .append("circle")
+		   .attr("class",function(d,i){
+		   	 return "m_circle" + " " + "circle" +i;
+		   })
+		   .attr("cx",function(d){
+		   		return d.x;
+		   })
+		   .attr("cy",function(d){
+		   		return d.y;
+		   })
+		   .attr("r","0.3em")
+		   .style("opacity",0);
 
 	title_g.selectAll(".m_circle")  
 		   .transition()
@@ -157,12 +204,14 @@ function first_drawing(num){
 		   		return circle_coord[index].y
 		   });
 
-	animation();
+	//animation();
 
 }
 
 
 function push_circle(num){
+
+	total_num+= num;
 
 	for(i=0;i<num;i++){
 		var temp ={};
@@ -171,11 +220,24 @@ function push_circle(num){
 		circle_coord.push(temp);
 	}
 
+	var index =0;
+	for(i=0; i<total_num; i++){
+	 	for(j=i+1; j<total_num; j++){
+	 		var temp = {};
+	 		temp.s = i;
+	 		temp.e = j;
+	 		line_coord[index]= temp;
+	 		index+=1;
+	 	}
+	 }
+
 	title_g.selectAll("circle")
 		.data(circle_coord)
 		.enter()
 		.append("circle")
-		.attr("class","m_circle")
+	    .attr("class",function(d,i){
+	   	 	return "m_circle" + " " + "circle" +i;
+	   })
 	   .attr("cx",function(d){
 	   		return d.x;
 	   })
@@ -183,6 +245,28 @@ function push_circle(num){
 	   		return d.y;
 	   })
 	   .attr("r","0.5em");
+
+	 title_g.selectAll("line")
+		   .data(line_coord)
+		   .enter()
+		   .append("line")
+		   .attr("class","m_line")
+		   .attr("x1",function(d){
+		   		var index = d.s;
+		   		return circle_coord[index].x
+		   })
+		   .attr("y1",function(d){
+		   		var index = d.s;
+		   		return circle_coord[index].y
+		   })
+		   .attr("x2",function(d){
+		   		var index = d.e;
+		   		return circle_coord[index].x
+		   })
+		   .attr("y2",function(d){
+		   		var index = d.e;
+		   		return circle_coord[index].y
+		   });
 }
 
 function animation(){
